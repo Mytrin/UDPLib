aa791fbbc8e49c25ce2419c11e54520e03d9b1ab
Martin Lepeska <martin.lepeska@gmail.com>
2016-04-11 16:36:47 +0200

    Config loading

---
+++ UDPLib/src/com/gmail/lepeska/martin/udplib/ConfigLoader.java
@@ -0,0 +1,73 @@
+package com.gmail.lepeska.martin.udplib;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * Class making easier XML files handling and loading basic library configuration.
+ *
+ * @author Martin Lepeška
+ */
+public final class ConfigLoader {
+    /** Indicator that some configuration was already loaded */
+     private static boolean configLoaded = false;
+     /** Loaded values */
+     private static HashMap<String, String> CONFIG;
+     /** Singleton */
+     private ConfigLoader(){}
+     
+    /**
+     * Loads and sets new configuration from "udplib_config.xml".
+     * @return true, if there was no exception, when parsing xml
+     */
+    public synchronized static final boolean loadConfig(){
+        try{
+            CONFIG = new HashMap<>();
+            loadConfig(new File("udplib_config.xml"));
+        }catch(Exception e){
+            Logger.getLogger(ConfigLoader.class.getName()).log(Level.SEVERE, "Config not loaded! ", e);
+        }
+        
+        return configLoaded;
+    }
+     
+    /**
+     * Loads and sets new configuration.
+     * @param source config file 
+     * 
+     * @throws java.lang.Exception 
+     */
+    public synchronized static final void loadConfig(File source) throws Exception{
+            Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(source);
+
+            NodeList configValues = doc.getDocumentElement().getElementsByTagName("item");
+
+            for (int i = 0; i < configValues.getLength(); i++) {
+                Node tag = configValues.item(i);
+                CONFIG.put(tag.getAttributes().getNamedItem("name").getTextContent(), tag.getTextContent());
+            }
+            
+            configLoaded = true;
+    } 
+    
+    /**
+     * @return true, if config was loaded
+     */
+    public synchronized static boolean isConfigLoaded() {
+        return configLoaded;
+    }
+
+    /**
+     * @param itemName name of tag
+     * @return its value or null
+     */
+    public synchronized String getValue(String itemName){
+        return CONFIG.get(itemName);
+    }
+}
